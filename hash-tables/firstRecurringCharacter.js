ffb946d965ef9534ba028d4513062ea87660e4a5
Totoblak90 <tobias.blaksley@hotmail.com>
2022-06-29 22:13:23 -0300

    end has-tables

---
+++ hash-tables/firstRecurringCharacter.js
@@ -0,0 +1,35 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+
+function firstRecurringCharacter(input) {
+    const obj = new Set();
+
+    for (let i = 0; i < input.length; i++) {
+        if (obj.has(input[i])) {
+            return input[i];
+        } else {
+            obj.add(input[i])
+        }
+        console.log(obj)
+    }
+    return undefined;
+}
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
+
+console.log(
+    firstRecurringCharacter([0, 5, 1, 0, 3, 5, 1, 2, 4]),
+    firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]),
+    firstRecurringCharacter([2, 3, 4, 5]),
+    firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]),
+)
